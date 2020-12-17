5926dc378e94be7a1f218db5053e841d01a1ed98
James Jelenko <jamesjelenko@gmail.com>
2020-12-16 17:31:09 -0800

    update advclient for testing

--- advclient.py
+++ advclient.py
@@ -3,7 +3,15 @@ from Tkinter import Label, Frame
 import datetime
 import requests
 import time
-IP_ADDR ="192.168.1.74"
+
+args = sys.argv
+bedroom_ip = args[1]
+livingroom_ip = args[2]
+port = args[3]
+COMMANDS = {}
+COMMANDS.OFF = 'off'
+COMMANDS.OFF = 'on'
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
@@ -19,64 +27,96 @@ class Application(tk.Frame):
         print('Exception: {}'.format(e))
         self.write_to_log(e)
 
-    def lights_off(self):
+    # TODO Nix when handler works
+    # def lights_off(self, ip):
+    #     try:
+    #         requests.get('http://{}:{}}/lights-off'.format(ip, port))
+    #     except Exception as e:
+    #         self.handle_errors(e)
+
+    # def lights_on(self, ip):
+    #     try:
+    #         requests.get('http://{}:{}}/lights-on'.format(ip, port))
+    #     except Exception as e:
+    #         self.handle_errors(e)
+    
+    def light_handler(self, ip, command):
         try:
-            requests.get('http://{}:8080/lights-off'.format(IP_ADDR))
+            requests.get('http://{}:{}}/lights-{}'.format(ip, port, command))
         except Exception as e:
             self.handle_errors(e)
 
-    def lights_on(self):
-        try:
-            requests.get('http://{}:8080/lights-on'.format(IP_ADDR))
-        except Exception as e:
-            self.handle_errors(e)
-    def start_motion(self):
-	try:
-	    print("starting motion sensor in 30 seconds")
-	    time.sleep(30)
-	    print("starting")
-	    requests.get('http://{}:5000/start_motion_sensor'.format(IP_ADDR))
-	except Exception as e:
-	    self.handle_errors(e)
+    def bedroom_lights_on(self):
+        light_handler(bedroom_ip, COMMANDS.ON)
 
-    def stop_motion(self):
-	try:
-	    print("Stopping motion sensors")
-	    requests.get('http://{}:5000/stop_motion_sensor'.format(IP_ADDR))
-	except Exception as e:
-	    self.handle_errors(e)
+    def bedroom_lights_off(self):
+        light_handler(bedroom_ip, COMMANDS.OFF)
+
+    def livingroom_lights_on(self):    
+        light_handler(livingroom_ip, COMMANDS.ON)
+
+    def livingroom_lights_off(self):
+        light_handler(livingroom_ip, COMMANDS.OFF)
+
+            #TODO Reimplement this when PIRs back in place
+ #    def start_motion(self):
+    # try:
+    #     print("starting motion sensor in 30 seconds")
+    #     time.sleep(30)
+    #     print("starting")
+    #     requests.get('http://{}:5000/start_motion_sensor'.format(IP_ADDR))
+    # except Exception as e:
+    #     self.handle_errors(e)
+
+ #    def stop_motion(self):
+    # try:
+    #     print("Stopping motion sensors")
+    #     requests.get('http://{}:5000/stop_motion_sensor'.format(IP_ADDR))
+    # except Exception as e:
+    #     self.handle_errors(e)
 
 
     def create_widgets(self):
-        self.lights_off_button = tk.Button(self, padx='50', pady='25')
-        self.lights_on_button = tk.Button(self, padx='50', pady='25')
-
-        self.motion_off_button = tk.Button(self, padx='50', pady='25')
-        self.motion_on_button = tk.Button(self, padx='50', pady='25')
-
-	self.motion_on_button["text"] = "Motion On"
-	self.motion_on_button["command"] = self.start_motion
+        self.bedroom_lights_off_button = tk.Button(self, padx='50', pady='25')
+        self.bedroom_lights_on_button = tk.Button(self, padx='50', pady='25')
+        self.livingroom_lights_off_button = tk.Button(self, padx='50', pady='25')
+        self.livingroom_lights_on_button = tk.Button(self, padx='50', pady='25')
+        # self.motion_off_button = tk.Button(self, padx='50', pady='25')
+        # self.motion_on_button = tk.Button(self, padx='50', pady='25')
 
 
-	self.motion_off_button["text"] = "Motion Off"
-	self.motion_off_button["command"] = self.stop_motion
+
+    self.livingroom_lights_off_button["text"] = "Living room OFF"
+    self.livingroom_lights_off_button["command"] = self.livingroom_lights_off
+
+    self.livingroom_lights_on_button["text"] = "Living room ON"
+    self.livingroom_lights_on_button["command"] = self.livingroom_lights_on
+
+    self.bedroom_lights_off_button["text"] = "Bedroom OFF"
+    self.bedroom_lights_off_button["command"] = self.bedroom_lights_off
+
+    self.bedroom_lights_on_button["text"] = "Bedroom ON"
+    self.bedroom_lights_on_button["command"] = self.bedroom_lights_on
 
 
-	self.lights_off_button["text"] = "Lights Off"
-        self.lights_off_button["command"] = self.lights_off
+    # self.motion_on_button["text"] = "Motion On"
+    # self.motion_on_button["command"] = self.start_motion
 
-	self.lights_on_button["text"] = "Lights On"
-        self.lights_on_button["command"] = self.lights_on
 
-	self.lights_on_button.pack(side="top")
-        self.lights_off_button.pack(side="top")
-        self.motion_off_button.pack(side="top")
-        self.motion_on_button.pack(side="top")
+    # self.motion_off_button["text"] = "Motion Off"
+    # self.motion_off_button["command"] = self.stop_motion
 
-	self.quit = tk.Button(self, text="QUIT", fg="red",
-                                  command=self.master.destroy,
-                                  padx='50', pady='50')
-        self.quit.pack(side="bottom")
+
+    self.livingroom_lights_off_button.pack(side="top")
+    self.livingroom_lights_on_button.pack(side="top")
+    self.bedroom_lights_off_button.pack(side="top")
+    self.bedroom_lights_on_button.pack(side="top")
+    # self.motion_off_button.pack(side="top")
+    # self.motion_on_button.pack(side="top")
+
+    self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy, padx='50', pady='50')
+    
+    self.quit.pack(side="bottom")
 
 root = tk.Tk()
 root.title('Light Controls')
