97b983bc83fc00f279ff422ca7d3937a796fb846
James Jelenko <jamesjelenko@gmail.com>
2021-11-11 09:32:25 -0800

    add logging, ip arg, clarify endpoints

--- lightserver.js
+++ lightserver.js
@@ -1,29 +1,40 @@
 const express = require('express');
 const g = require('onoff').Gpio
 const app = express();
+const logger = require('tracer').colorConsole();
 
 const args = process.argv.slice(2);
 const room = args[0]
 const port = args[1]
 const pin = args[2]
+const ip = args[3]
+
 const lamp = new g(pin, 'out');
 
+const log = (data) => {
+    let d = new Date();
+    let date = d.toLocaleDateString();
+    let time = d.toLocaleTimeString();
+    logger.info(`${room} lights for ${ip} went ${data} at ${time} on ${date}.\n`);
+}
 
 const on = () => {
     lamp.writeSync(1);
+    log('on');
 }
 
 const off = () => {
     lamp.writeSync(0);
+    log('off');
 }
 
-app.get('/lights-on', (req, res) => {
+app.get('/on', (req, res) => {
     on();
-    res.send(`${room} lights ack'd: on`);
+    res.send(`${room} lights ack'd: on - IP: ${ip} - Pin #${pin}\n`);
 })
-app.get('/lights-off', (req, res) => {
+app.get('/off', (req, res) => {
     off();
-    res.send(`${room} lights ack'd: off`);
+    res.send(`${room} lights ack'd: off - IP: ${ip} - Pin #${pin}\n`);
 })
 app.listen(port, ()=> {
     console.log(`Listening on ${port}`);
